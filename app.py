219b240a4e3bebdbfa4e1df631b3cb2553c1da6d
dariohhhyryt <darioarcanjo32@gmail.com>
2025-06-01 03:13:39 +0000

    adicionar o projecto

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 
 
 app = Flask(__name__)
 
+VAGAS = [{
+    'id': 1,
+    'titulo': 'analista de dados',
+    'local': 'Nampula',
+    'salario': '10000'
+},
+        
+         
+ ]
+
 @app.route("/")
-def hello():
-    return  "hello world"
+def home():
+    return  render_template('home.html', vagas=VAGAS)
+
+@app.route("/vagas")
+def lista_vagas():
+    return jsonify(VAGAS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
