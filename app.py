70fd391326f0758eee74b99fc8e10eb904f632ad
dariohhhyryt <darioarcanjo32@gmail.com>
2025-07-06 11:08:52 +0000

    adicionar o projecto

--- app.py
+++ app.py
@@ -1,32 +1,28 @@
-from flask import Flask, jsonify, render_template, request
-from database import carregar_vagas_db, carregar_vaga_db, formulario_vaga
+from flask import Flask, render_template,jsonify
+
 
 
 app = Flask(__name__)
 
+VAGAS = [
 
+    {'id': 1, 'nome': 'celular', 'codigo': '4565655', 'preco': '20150mt', 'marca': 'nike'},
+    {'id': 2, 'nome': 'Desktop', 'codigo': '4565655', 'preco': '22250mt', 'marca': 'Solo'},
+    {'id': 3, 'nome': 'Leptop', 'codigo': '4565655', 'preco': '45050mt', 'marca': 'nike'},
+    {'id': 4, 'nome': 'Monitor', 'codigo': '4565655', 'preco': '5550mt', 'marca': 'adidas'}
+
+    
+]
 
 @app.route("/")
 def home():
-    vagas = carregar_vagas_db()
-    return  render_template('home.html', vagas=vagas)
+    return render_template("home.html",vagas=VAGAS)
 
 @app.route("/vagas")
 def lista_vagas():
-    vagas = carregar_vagas_db()
-    return jsonify(vagas)
+    return jsonify(VAGAS)
 
-@app.route("/vaga/<id>")
-def mostra_vaga(id):
-    vaga = carregar_vaga_db(id)
-    return render_template('detalhevaga.html', vaga=vaga)
 
-@app.route("/vaga/<id>/formulario", methods=["GET", "POST"])
-def formulario_vaga(id):
-   vaga = carregar_vaga_db(id)
-   data = request.form
-   formulario_vaga(id, data)
-   return render_template('formulario_vaga.html', vaga=vaga, formulario = data)
 
 
 if __name__ == "__main__":
